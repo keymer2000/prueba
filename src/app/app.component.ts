f183031076bb7d84ce89e97b9122d2ebb1a98a36
keymerblack <keimeralexanderj@gmail.com>
2022-02-03 20:32:29 -0500

    complementaria

---
+++ src/app/app.component.ts
@@ -0,0 +1,109 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'portfolio';
+  public Certificados = [
+    {
+      titulo: 'Fundamentos SCRUM',
+      cuerpo: '40 horas, Sophos Solutions Medellín, 2021'
+    },
+    {
+      titulo: 'Conceptos Básicos de Devops',
+      cuerpo: '40 horas, Sophos Solutions Medellín, 2021'
+    },
+    {
+      titulo: 'AWS Fundamentals - Practitioner',
+      cuerpo: '40 horas, Sophos Solutions Medellín, 2021'
+    },
+    {
+      titulo: 'Desarrollo Seguro',
+      cuerpo: '40 horas, Sophos Solutions Medellín, 2021'
+    },
+    {
+      titulo: 'Documentación de un sistema de gestión de la calidad - NTC ISO 9001',
+      cuerpo: '40 horas, servicio nacional de aprendizaje (SENA) Chía, 2020.'
+    },
+    {
+      titulo: 'Evaluación y mejora de un sistema de gestión de la calidad - NTC ISO 9001',
+      cuerpo: '40 horas, servicio nacional de aprendizaje (SENA) Ibagué, 2020.'
+    },
+    {
+      titulo: 'Planificación de un sistema de gestión de la calidad - NTC ISO 9001',
+      cuerpo: '40 horas, servicio nacional de aprendizaje (SENA) Ibagué, 2020.'
+    },
+    {
+      titulo: 'Modelos de calidad de software',
+      cuerpo: '40 horas, servicio nacional de aprendizaje (SENA) Bogotá, 2020.'
+    },
+    {
+      titulo: 'Bases de datos generalidades y sistemas de gestión',
+      cuerpo: '40 horas, servicio nacional de aprendizaje (SENA) Neiva, 2020.'
+    },
+    {
+      titulo: 'Auditoria informática conceptualización',
+      cuerpo: '40 horas, servicio nacional de aprendizaje (SENA) Barranquilla, 2020.'
+    },
+    {
+      titulo: 'Redes y Seguridad',
+      cuerpo: '40 horas, servicio nacional de aprendizaje (SENA) Popayán, 2020.'
+    },
+    {
+      titulo: 'Controles y seguridad informática',
+      cuerpo: '40 horas, servicio nacional de aprendizaje (SENA) Bogotá, 2020.'
+    },
+    {
+      titulo: 'Procesos de soporte técnico para el mantenimiento de equipos de cómputo',
+      cuerpo: '40 horas, servicio nacional de aprendizaje (SENA) Bogotá, 2020.'
+    },
+    {
+      titulo: 'Estrategias para el acceso y uso de la información',
+      cuerpo: '40 horas, servicio nacional de aprendizaje (SENA) Bogotá, 2020.'
+    },
+    {
+      titulo: 'Seminario inducción al mundo laboral',
+      cuerpo: '10 horas, Politécnico Mayor Medellín, 2019.'
+    },
+    {
+      titulo: 'Emprendimiento empresarial',
+      cuerpo: '40 horas, servicio nacional de aprendizaje (SENA)Aguachica, 2017.'
+    },
+    {
+      titulo: 'Ciberseguridad ciudadana',
+      cuerpo: 'Jóvenes programadores Chile, 2020.'
+    },
+    {
+      titulo: 'Curso CSS',
+      cuerpo: 'Jóvenes programadores Chile, 2020.'
+    },
+    {
+      titulo: 'Curso JavaScript 1',
+      cuerpo: 'Jóvenes programadores Chile, 2020.'
+    },
+    {
+      titulo: 'Curso Scratch introductorio',
+      cuerpo: 'Jóvenes programadores Chile, 2020.'
+    },
+    {
+      titulo: 'Data Science Foundations - Level 1',
+      cuerpo: 'issued on: 01 Jun 2020 issued by: IBM'
+    },
+    {
+      titulo: 'Curso inteligencia artificial nivel básico',
+      cuerpo: '30 horas, alcaldía de Bogotá, 2018.'
+    },
+    {
+      titulo: 'Curso inteligencia artificial nivel medio',
+      cuerpo: '30 horas, alcaldía de Bogotá, 2018.'
+    },
+    {
+      titulo: 'Comunicación en ingles pre-a1/pre principiante',
+      cuerpo: '60 horas, Aguachica, 2017'
+    },
+    
+  ];
+}
